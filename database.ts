7fc11bcf2f0c290f7115a7a8572840fdad9dc0e3
Andrew Wittrock <andy@wittrock.us>
2025-02-12 15:59:03 -0600

    Only allow one database instance

--- database.ts
+++ database.ts
@@ -1,4 +1,5 @@
 import knex, { Knex } from "knex";
 import { dbConfig } from '../../config';
+import { memoize } from "ts-functional";
 
-export const database = () => knex(dbConfig as Knex.Config);
+export const database = memoize(() => knex(dbConfig as Knex.Config), {});
