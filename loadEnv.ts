4e3f1501bf67343a316c12d806a8bb114a6df5fc
Andrew Wittrock <andy@wittrock.us>
2026-02-24 14:21:03 -0600

    Remove quotes from environment variables when loading

--- loadEnv.ts
+++ loadEnv.ts
@@ -5,10 +5,18 @@ export const loadEnv = (envFile:string):Index<string> => fs.readFileSync(envFile
     .split('\n')
     .filter(line => line.trim() !== '' && !line.startsWith('#'))
     .reduce((acc, line) => {
-        const [key, value] = line.split('=');
-        if(['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_REGION', 'AWS_DEFAULT_REGION'].includes(key.trim())) {
+        const eqIndex = line.indexOf('=');
+        if (eqIndex === -1) return acc;
+        const key = line.substring(0, eqIndex).trim();
+        let value = line.substring(eqIndex + 1).trim();
+
+        if(['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_REGION', 'AWS_DEFAULT_REGION'].includes(key)) {
             return acc;
         }
-        acc[key.trim()] = value.trim();
+
+        // Strip quotes
+        value = value.replace(/^(['"])(.*)\1$/, '$2');
+
+        acc[key] = value;
         return acc;
     }, {} as Index<string>);
